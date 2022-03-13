857879bb47f1c3f63056df9a986f3575f3268987
Abdi Temesgen <abditemesgen233@gmail.com>
2022-03-13 01:54:46 -0800

    For variables task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always  0 (Success/correct)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
