a0b4baaa926a625eafd2f3f1ad094278aafad9b3
Abdi Temesgen <abditemesgen233@gmail.com>
2022-03-14 14:13:39 -0700

    For variables task _3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
-		putchar(lower_case);
+		putchar (lower_case);
 	}
 
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
-		putchar(upper_case);
+		putchar (upper_case);
 	}
 	return(0);
 }
